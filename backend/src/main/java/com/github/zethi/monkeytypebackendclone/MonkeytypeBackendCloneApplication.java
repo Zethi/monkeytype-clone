f09dbbde655ca1446718b7938edab3dd901d5eb8
Zethi <diego1900196@gmail.com>
2024-01-12 13:59:15 -0300

    Backend initial commit

---
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/MonkeytypeBackendCloneApplication.java
@@ -0,0 +1,13 @@
+package com.github.zethi.monkeytypebackendclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeytypeBackendCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeytypeBackendCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/zethi/monkeytypebackendclone/MonkeytypeBackendCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zethi.monkeytypebackendclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeytypeBackendCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
