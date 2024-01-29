ca9285eb44ad829f9930163881c6c1af9f82322b
Zethi <diego1900196@gmail.com>
2024-01-29 10:47:32 -0300

    modify: Renaming main class

--- backend/src/main/java/com/github/zethi/monkeytypebackendclone/MonkeyTypeBackendCloneApplication.java
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/MonkeyTypeBackendCloneApplication.java
@@ -3,11 +3,11 @@ package com.github.zethi.monkeytypebackendclone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MonkeytypeBackendCloneApplication {
+@SpringBootApplication()
+public class MonkeyTypeBackendCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonkeytypeBackendCloneApplication.class, args);
+		SpringApplication.run(MonkeyTypeBackendCloneApplication.class, args);
 	}
 
 }
