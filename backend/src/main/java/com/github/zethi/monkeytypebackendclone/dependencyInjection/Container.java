d7145bc660d12e0075c2f1971c64eb3f0df537b2
Zethi <diego1900196@gmail.com>
2024-01-29 10:48:26 -0300

    add: DependencyInjection Bean container

---
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/dependencyInjection/Container.java
@@ -0,0 +1,36 @@
+package com.github.zethi.monkeytypebackendclone.dependencyInjection;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.zethi.monkeytypebackendclone.entity.JSON;
+import com.github.zethi.monkeytypebackendclone.repositorys.DictionaryRepository;
+import com.github.zethi.monkeytypebackendclone.repositorys.JSONDictionaryRepository;
+import com.github.zethi.monkeytypebackendclone.services.FileService;
+import com.github.zethi.monkeytypebackendclone.utils.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class Container {
+
+    @Bean
+    public ObjectMapper instanceObjectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public JsonParser instanceJsonParser(@Autowired ObjectMapper objectMapper) {
+        return new JsonParser(objectMapper);
+    }
+
+    @Bean
+    public JSON JSON(@Autowired ObjectMapper objectMapper) {
+        return new JSON(objectMapper);
+    }
+
+    @Bean
+    public DictionaryRepository dictionaryRepository(@Autowired FileService fileService, @Autowired JsonParser jsonParser, @Autowired ObjectMapper objectMapper) {
+        return new JSONDictionaryRepository("data/dictionaries", fileService, jsonParser, objectMapper);
+    }
+
+}
