e7a6bbb8044c13e8d40e6321382ec01843f1f9c1
Zethi <diego1900196@gmail.com>
2024-01-29 10:47:05 -0300

    add: JSON parser utility

---
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/utils/JsonParser.java
@@ -0,0 +1,41 @@
+package com.github.zethi.monkeytypebackendclone.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.zethi.monkeytypebackendclone.entity.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JsonParser {
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public JsonParser(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public <T> T jsonToClass(JSON json, Class<T> type) throws JsonProcessingException {
+        return objectMapper.readValue(json.toString(), type);
+    }
+
+    public <T> JSON classToJson(T instance) throws JsonProcessingException {
+        String rawJson = objectMapper.writeValueAsString(instance);
+        JsonNode nodeJson = objectMapper.readTree(rawJson);
+
+        JSON json = new JSON(objectMapper, nodeJson);
+
+        return json;
+    }
+
+    public JSON stringToJson(String data) throws JsonProcessingException {
+        JSON json = new JSON(objectMapper, objectMapper.readTree(data));
+
+        return json;
+    }
+
+
+}
