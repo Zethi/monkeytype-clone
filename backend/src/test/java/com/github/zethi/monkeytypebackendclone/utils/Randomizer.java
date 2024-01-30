d8afab707fc6f80aeeca9f8a0a33d5827903a961
Zethi <diego1900196@gmail.com>
2024-01-30 12:35:59 -0300

    add: Util to create random String and String array

---
+++ backend/src/test/java/com/github/zethi/monkeytypebackendclone/utils/Randomizer.java
@@ -0,0 +1,31 @@
+package com.github.zethi.monkeytypebackendclone.utils;
+
+import java.util.Random;
+
+public final class Randomizer {
+
+
+    public static String[] generateRandomStringArray(int arrayLength, int stringLength) {
+        String[] randomStrings = new String[arrayLength];
+        Random random = new Random();
+
+        for (int i = 0; i < arrayLength; i++) {
+            randomStrings[i] = generateRandomString(stringLength);
+        }
+
+        return randomStrings;
+    }
+
+    public static String generateRandomString(int length) {
+        String allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder randomString = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = new Random().nextInt(allowedCharacters.length());
+            char randomChar = allowedCharacters.charAt(randomIndex);
+            randomString.append(randomChar);
+        }
+
+        return randomString.toString();
+    }
+}
