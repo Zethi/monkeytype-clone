d6161994b9481be90db1d8e1973f84b7342f5617
Zethi <diego1900196@gmail.com>
2024-02-04 12:37:49 -0300

    add: Scope to beans

--- backend/src/main/java/com/github/zethi/monkeytypebackendclone/dependencyInjection/Container.java
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/dependencyInjection/Container.java
@@ -7,23 +7,28 @@ import com.github.zethi.monkeytypebackendclone.repositorys.JSONDictionaryReposit
 import com.github.zethi.monkeytypebackendclone.services.FileService;
 import com.github.zethi.monkeytypebackendclone.utils.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class Container {
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ObjectMapper instanceObjectMapper() {
         return new ObjectMapper();
     }
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public JSON JSON(@Autowired ObjectMapper objectMapper) {
         return new JSON(objectMapper);
     }
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public DictionaryRepository dictionaryRepository(@Autowired FileService fileService, @Autowired JsonParser jsonParser, @Autowired ObjectMapper objectMapper) {
         return new JSONDictionaryRepository("data/dictionaries", fileService, jsonParser, objectMapper);
     }
