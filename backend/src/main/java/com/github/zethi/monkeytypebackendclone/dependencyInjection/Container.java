3cdb9d54e664d3885c8047d52808f04af590bd60
Zethi <diego1900196@gmail.com>
2024-03-13 21:09:38 -0300

    modify: Fix JavaTimeModule in ObjectMapper and custom path for dictionaries from env at DictionaryRepository

--- backend/src/main/java/com/github/zethi/monkeytypebackendclone/dependencyInjection/Container.java
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/dependencyInjection/Container.java
@@ -1,24 +1,37 @@
 package com.github.zethi.monkeytypebackendclone.dependencyInjection;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.zethi.monkeytypebackendclone.entity.JSON;
 import com.github.zethi.monkeytypebackendclone.repositorys.DictionaryRepository;
-import com.github.zethi.monkeytypebackendclone.repositorys.JSONDictionaryRepository;
+import com.github.zethi.monkeytypebackendclone.repositorys.implementation.JSONDictionaryRepositoryImpl;
 import com.github.zethi.monkeytypebackendclone.services.FileService;
 import com.github.zethi.monkeytypebackendclone.utils.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class Container {
 
+    private final Environment environment;
+
+    @Autowired
+    public Container(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ObjectMapper instanceObjectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+
+        return objectMapper;
     }
 
     @Bean
@@ -30,7 +43,7 @@ public final class Container {
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public DictionaryRepository dictionaryRepository(@Autowired FileService fileService, @Autowired JsonParser jsonParser, @Autowired ObjectMapper objectMapper) {
-        return new JSONDictionaryRepository("data/dictionaries", fileService, jsonParser, objectMapper);
+        return new JSONDictionaryRepositoryImpl(environment.getProperty("dictionaries.path"), fileService, jsonParser, objectMapper);
     }
 
 }
