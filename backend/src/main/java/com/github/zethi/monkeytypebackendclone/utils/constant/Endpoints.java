d15c12b30c4014f331890a83d037e0efef6cb853
Zethi <diego1900196@gmail.com>
2024-03-13 21:15:16 -0300

    add: Public Endpoints list

---
+++ backend/src/main/java/com/github/zethi/monkeytypebackendclone/utils/constant/Endpoints.java
@@ -0,0 +1,10 @@
+package com.github.zethi.monkeytypebackendclone.utils.constant;
+
+public class Endpoints {
+    public static final String[] PUBLIC_ENDPOINTS = {
+            "/api/dictionary/**",
+            "/api/auth/login",
+            "/api/auth/signup",
+            "/api/auth/forgot-password"
+    };
+}
